ddea1b233bd548de508efc7308b5409ec7a47b31
Muhammad_Naeem <naeem48916@gmail.com>
2024-02-28 17:41:36 +0500

    setup and requirements

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup
+from typing  import List
+
+hypen='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return  the requirements list from the given file path.
+
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","")for req in requirements]
+        if hypen in requirements:
+            requirements.remove(hypen)
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Muhammad_Naeem',
+    author_email='naeem48916@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
